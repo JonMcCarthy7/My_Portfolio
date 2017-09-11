735d3550ed9095d4ce1c1970799e1b702bbb5e7e
JonMcCarthy7 <jtmcc93@gmail.com>
2017-09-11 15:13:27 -0700

    style and add myAge function

--- scripts/script.js
+++ scripts/script.js
@@ -2,89 +2,85 @@ $(document).ready(function(){
 
  $('#trans').on('click', function () {
    $(this).fadeOut(function () {
-     $(this).text(($(this).text() ==  "Herzlich willkommen auf meiner Website,") ? "Welcome to my website," : "Herzlich willkommen auf meiner Website,").fadeIn();
+     $(this).text(($(this).text() ==  "Herzlich willkommen auf meiner portfolio,") ? "Welcome to my portfolio," : "Herzlich willkommen auf meiner portfolio,").fadeIn();
    })
  });
 
-function myAge() {
-  var d = new Date();
-  var n = d.getFullYear() - 1993;
-  document.getElementById('age').innerHTML = n;
-  return n;
-}
+  function myAge(jsYear, jsMonth, jsDate) {
+    var d = new Date();
+    var n = d.getFullYear() - jsYear;
+    if (d.getMonth() >= jsMonth && d.getDate() >= jsDate) {
+      n+= 1;
+    };
+    return n;
+  }
 
-    myAge();
+  document.getElementById('age').innerHTML = myAge(1993, 4, 19);
 
-    $('#typeIt').typeIt({
-      breakLines: false,
-      speed: 50,
-      autoStart: false,
-      startDelay: 1000
-    })
-    .tiType('Full-Stack Developer')
-    .tiPause(2000)
-    .tiDelete()
-    .tiType('Coffee Connoisseur')
-    .tiPause(2000)
-    .tiDelete()
-    .tiType('Musician')
-    .tiPause(2000)
-    .tiDelete()
-    .tiType('Food Lover')
-    .tiPause(2000)
-    .tiDelete()
-    .tiType('Traveler')
-    .tiPause(2000)
-    .tiDelete()
-    .tiType('Thrill Seeker')
-    .tiPause(2000)
-    .tiDelete()
-    .tiType('Full-Stack Developer');
-
-
-    $('#example2').typeIt({
-     strings: ["This is a great string.", "But here is a better one."],
-     speed: 50,
-     breakLines: false,
-     autoStart: false
-});
+  $('#typeIt').typeIt({
+    breakLines: false,
+    speed: 50,
+    autoStart: false,
+    startDelay: 1000
+  })
+  .tiType('Full-Stack Developer')
+  .tiPause(2000)
+  .tiDelete()
+  .tiType('Coffee Connoisseur')
+  .tiPause(2000)
+  .tiDelete()
+  .tiType('Musician')
+  .tiPause(2000)
+  .tiDelete()
+  .tiType('Food Lover')
+  .tiPause(2000)
+  .tiDelete()
+  .tiType('Traveler')
+  .tiPause(2000)
+  .tiDelete()
+  .tiType('Adrenaline Junkie')
+  .tiPause(2000)
+  .tiDelete()
+  .tiType('Full-Stack Developer');
 
 
 
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
-        }
-      }
-    });
+
+    // Smooth Scroll
+      // Select all links with hashes
+      $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+          // On-page links
+          if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+          ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+              // Only prevent default if animation is actually gonna happen
+              event.preventDefault();
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000, function() {
+                // Callback after animation
+                // Must change focus!
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                  return false;
+                } else {
+                  $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                  $target.focus(); // Set focus again
+                };
+              });
+            }
+          }
+        });
 });
